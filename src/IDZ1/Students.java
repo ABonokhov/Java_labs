ab2a776d8e4440fe5a770823b6141a7bb62fa7fc
ABonokhov <43575362+ABonokhov@users.noreply.github.com>
2021-03-01 11:51:03 +0300

    Add files via upload

---
+++ src/IDZ1/Students.java
@@ -0,0 +1,48 @@
+package IDZ1;
+
+public class Students {
+    private String surname;
+    private int age;
+    private int debts; // долги по учебе
+
+    public Students(String surname, int age, int debts) {
+        super();
+        this.surname = surname;
+        this.age = age;
+        this.debts = debts;
+    }
+
+    public Students() {
+
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getDebts() {
+        return debts;
+    }
+
+    public void setDebts(int debts) {
+        this.debts = debts;
+    }
+    public void show () {
+        System.out.println("Surname  = " + surname);
+        System.out.println("Age = " + age);
+        System.out.println("Debts = " + debts);
+    }
+    }
+
