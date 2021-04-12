1eb8ef02ae1144f363811b274d2390220fab08ec
Luckytoha333@gmail.com <Anton051570>
2021-04-12 10:33:02 +0300

    comm2

--- src/by/gsu/pms/Main.java
+++ src/by/gsu/pms/Main.java
@@ -1,8 +1,36 @@
 package by.gsu.pms;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        Students[] student = {
+                new Students("Baklaev", 19, 2),
+                new Students("Malinin", 20, 4),
+                new Students("Piraeva", 18, 0),
+                null,
+                new Students("Kruchilin", 21, 2),
+                null,
+                null,
+                new Students("Voronov", 20, 0),
+                new Students("Golovin", 21, 1),
+                new Students("Morozova", 19, 0),
+        };
+        int totalAge=0;
+        int totalDebts=0;
+        for (Students students : student) {
+            if (students != null){
+                totalAge+=students.getAge();
+                students.show();
+                System.out.println("______________");
+                if (students.getDebts() != 0) {
+                    totalDebts++;
+                    System.out.println();
+                }
+            }
+        }
+        int avgAge=totalAge/student.length;
+        System.out.println("Average age of all students = " + avgAge);
+        System.out.println("Students with debts = " + totalDebts);
     }
 }
+
+
