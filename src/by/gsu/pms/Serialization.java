ec34abd29323b4cb6dca15219d3e0664d5d0ffc2
Luckytoha333@gmail.com <Anton051570>
2021-04-12 11:59:59 +0300

    comm_4(valueOf)

--- src/by/gsu/pms/Serialization.java
+++ src/by/gsu/pms/Serialization.java
@@ -3,9 +3,20 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
+import static java.lang.String.valueOf;
+
 public class Serialization{
     public void serialise(Students[]student) {
         ObjectOutputStream ostream = null;
+        try {
+            System.out.println("serialized");
+            ostream = new ObjectOutputStream(new FileOutputStream("D:\\java_labs\\IDZ3\\Students.dat"));
+            ostream.writeObject(student);
+            ostream.close();
+        } catch (IOException e) {
+            System.out.println(valueOf(e));
+        }
 
     }
-}
+
+    }
