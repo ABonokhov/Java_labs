890d917c960d22aca82544f374068ac5cba1d776
Luckytoha333@gmail.com <Anton051570>
2021-04-12 11:36:31 +0300

    comm3

---
+++ src/by/gsu/pms/Serialization.java
@@ -0,0 +1,11 @@
+package by.gsu.pms;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class Serialization{
+    public void serialise(Students[]student) {
+        ObjectOutputStream ostream = null;
+
+    }
+}
