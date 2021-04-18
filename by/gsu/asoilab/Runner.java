30affafab3cc419cbbd87e6f726d4fa6c8c45163
ABonokhov <43575362+ABonokhov@users.noreply.github.com>
2021-04-18 21:55:58 +0300

    Variant1_final

--- by/gsu/asoilab/Runner.java
+++ by/gsu/asoilab/Runner.java
@@ -1,15 +1,18 @@
-package by.gsu.asoilab;
+import by.gsu.asoilab.uniformMaterial1;
+import by.gsu.asoilab.uniformMaterial2;
 
 public class Runner {
     public static void main(String[] args) { 
     uniformMaterial1 steel = new uniformMaterial1("steel",7850);
-    uniformMaterial1 copper = new uniformMaterial1("copper",8500);
     uniformMaterial2 wire = new uniformMaterial2("wire",steel,0.03);
-    
-    System.out.println(steel.toString());
-	System.out.println(copper.toString());
     System.out.println(wire.toString());
-    System.out.println("Масса объекта = " + uniformMaterial2.getMass(steel));
+
+    wire.setMaterial(new uniformMaterial1("copper", 8500));
+    System.out.println(uniformMaterial2.getMass());
+
+    wire.setMaterial(steel);
+    System.out.println(wire.toString());
+    System.out.println("Масса объекта = " + uniformMaterial2.getMass());
 
     
 
