0fa92ab070caf7797c521d2fd1ac78567e68d467
ABonokhov <43575362+ABonokhov@users.noreply.github.com>
2021-11-02 22:17:23 +0300

    First_version

---
+++ src/Currency.java
@@ -0,0 +1,63 @@
+class Currency {
+    private String Name;
+    private String NumCode;
+    private String CharCode;
+    private String Scale;
+    private String Rate;
+    private int Id;
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getNumCode() {
+        return NumCode;
+    }
+
+    public String getCharCode() {
+        return CharCode;
+    }
+
+    public String getScale() {
+        return Scale;
+    }
+
+    public String getRate() {
+        return Rate;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setNumCode(String numCode) {
+        NumCode = numCode;
+    }
+
+    public void setCharCode(String charCode) {
+        CharCode = charCode;
+    }
+
+    public void setScale(String scale) {
+        Scale = scale;
+    }
+
+    public void setRate(String rate) {
+        Rate = rate;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Currency: " +
+                "Name='" + Name + '\'' +
+                ", NumCode='" + NumCode + '\'' +
+                ", CharCode='" + CharCode + '\'' +
+                ", Scale='" + Scale + '\'' +
+                ", Rate='" + Rate + '\'' +
+                ", Id='" + Id + '\'';
+    }
+}
